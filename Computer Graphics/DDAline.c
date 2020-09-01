2d5c9c2194ab38aade843136a6df7851dc0a6d83
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2020-09-02 00:15:03 +0530

    Update DDAline.c

--- Computer Graphics/DDAline.c
+++ Computer Graphics/DDAline.c
@@ -1,26 +1,26 @@
-// Still Working on the code
 #include<GL/glut.h>
 #include<GL/gl.h>
 #include<GL/glu.h>
 #include<stdlib.h>
 #include<stdio.h>
-float x1,x2,y1,y2;
+float x_1,x_2,y_1,y_2;
  
 void display()
 {
 	float dy,dx,x,y,m,steps,xin,yin;
 	int i;
-	dx=x2-x1;
-	dy=y2-y1;	
+	dx=x_2-x_1;
+	dy=y_2-y_1;	
 	m = dy/dx; 	
 	
-	x = x1;
-	y = y1;
+	x = x_1;
+	y = y_1;
 
 	glBegin(GL_POINTS);
-	glVertex2i(x1,y1);
+	glVertex2f(x,y);
 	glEnd();
 
+	//When calculated slope is less than 1 
 	if (m<1)
 	{
 		steps = dx;
@@ -28,36 +28,79 @@ void display()
 		yin = dy/steps;
 		for(i = 1;i<=steps;i++)
 		{
-			x = x + xin;
 			y = y + yin;
+			x = x + xin;
+			glBegin(GL_POINTS);
 			if( y > (abs(y) + 0.5))
 			{
-				y = abs(y)+1;
+				glVertex2f(x,abs(y)+1);
 			}
 			else
 			{
-				y = abs(y);
+				glVertex2f(x,abs(y));
 			}
-		
-			glBegin(GL_POINTS);
-			glVertex2i(x,y);
 			glEnd();
-		} 
+		}
 	}
- 
-	glFlush();
+
+	//When calculated slope is greater than 1
+	else if (m>1)
+	{
+		steps = dy;
+		xin = dx/steps;
+		yin = dy/steps;
+		for(i = 1;i<=steps;i++)
+		{
+			y = y + yin;
+			x = x + xin;
+			glBegin(GL_POINTS);
+			if( y > (abs(y) + 0.5))
+			{
+				glVertex2f(x,abs(y)+1);
+			}
+			else
+			{
+				glVertex2f(x,abs(y));
+			}
+			glEnd();
+		}
+	}
+
+	//When calculated slope is equal to 1
+	else if (m == 1)
+ 	{
+		steps = dy; // or steps = dx
+		xin = dx/steps;
+		yin = dy/steps;
+		for(i = 1;i<=steps;i++)
+		{
+			y = y + yin;
+			x = x + xin;
+			glBegin(GL_POINTS);
+			if( y > (abs(y) + 0.5))
+			{
+				glVertex2f(x,abs(y)+1);
+			}
+			else
+			{
+				glVertex2f(x,abs(y));
+			}
+			glEnd();
+		}
+		glFlush();
+	}
 }
  
 int main(int argc, char** argv) 
 {
 	printf("Enter the value of x1 : ");
-	scanf("%f",&x1);
+	scanf("%f",&x_1);
 	printf("Enter the value of y1 : ");
-	scanf("%f",&y1);
+	scanf("%f",&y_1);
 	printf("Enter the value of x2 : ");
-	scanf("%f",&x2);
+	scanf("%f",&x_2);
 	printf("Enter the value of y2 : ");
-	scanf("%f",&y2);
+	scanf("%f",&y_2);
 	 
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
